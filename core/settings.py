d127777d1647b60b05d258da96899c2a80b17aa6
Saydum <saydum.it5@gmail.com>
2024-01-05 20:35:38 +0300

    ...

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,7 +44,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
+        'DIRS': [[os.path.join(BASE_DIR, 'templates')]],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +119,3 @@ MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (
-    os.path.join(SETTINGS_PATH, 'templates'),
-)
