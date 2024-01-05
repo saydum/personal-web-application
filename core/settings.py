ed9b0dc497a4b6290749e3945b40ee2f9b75852f
Saydum <saydum.it5@gmail.com>
2024-01-05 20:37:43 +0300

    ...

--- core/settings.py
+++ core/settings.py
@@ -119,3 +119,5 @@ MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, '/templates/')]
