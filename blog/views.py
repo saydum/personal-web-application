4590af84d186087ffb6b1c903ae513896cb3ce97
Saydum <saydum.it5@gmail.com>
2024-01-05 20:43:54 +0300

    ...

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,12 @@ from django.views import generic
 from .models import Post
 
 def index(request):
-    return render(request, "blog/index.html", {})
+    return render(request, "/home/saydum/core/templates/blog/index.html", {})
 
 class PostList(generic.ListView):
     queryset = Post.objects.all()
-    template_name = 'blog/posts.html'
+    template_name = '/home/saydum/core/templates/blog/posts.html'
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'blog/post_deatail.html'
+    template_name = '/home/saydum/core/templates/blog/post_deatail.html'
